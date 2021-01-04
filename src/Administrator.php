41be04ed9ae657362f392e706841bfb42a2eb7f5
petr <petr@lqd.cz>
2021-01-04 14:50:45 +0100

    storage fix

--- src/Administrator.php
+++ src/Administrator.php
@@ -4,35 +4,37 @@ declare(strict_types=1);
 
 namespace Admin;
 
+use Nette\Security\Authorizator;
 use Nette\Security\IAuthenticator;
 use Nette\Security\IAuthorizator;
 use Nette\Security\IIdentity;
 use Nette\Security\IUserStorage;
+use Nette\Security\UserStorage;
 
 class Administrator extends \Nette\Security\User
 {
 	private ?string $defaultLink = null;
-	
-	public function __construct(IUserStorage $storage, ?IAuthenticator $authenticator = null, ?IAuthorizator $authorizator = null)
+
+	public function __construct(IAuthenticator $authenticator = null, Authorizator $authorizator = null, IUserStorage $legacyStorage = null)
 	{
-		parent::__construct(clone $storage, $authenticator, $authorizator);
-		
-		/** @var \Nette\Http\UserStorage $storage */
-		$storage = $this->getStorage();
-		$storage->setNamespace('admin');
+		parent::__construct(null, $authenticator, $authorizator, clone $legacyStorage);
+
+		/** @var \Nette\Http\UserStorage $legacyStorage */
+		$legacyStorage = $this->getStorage();
+		$legacyStorage->setNamespace('admin');
 	}
-	
+
 	public function setDefaultLink(string $defaultLink): void
 	{
 		$this->defaultLink = $defaultLink;
 	}
-	
+
 	public function getDefaultLink(): string
 	{
 		if (!$this->defaultLink) {
 			throw new \DomainException('Default link is not set');
 		}
-		
+
 		return $this->defaultLink;
 	}
 }
