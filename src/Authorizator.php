a08a3881645957fa2c3147fcbe82faab42d089c0
Oldrich <oldrich@lqd.cz>
2021-04-10 13:06:17 +0200

    authorizator

--- src/Authorizator.php
+++ src/Authorizator.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Admin;
 
 use Nette\Security\IAuthorizator;
-use Security\DB\PermissionRepository;
+use Admin\DB\PermissionRepository;
 
 class Authorizator implements IAuthorizator
 {
