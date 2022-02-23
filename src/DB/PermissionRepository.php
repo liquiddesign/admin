25c16b0094efbac4e426510a907b544c51f548ff
Oldrich <oldrich@lqd.cz>
2022-02-23 12:11:24 +0100

    fixed phpstan and error not allowed bug

--- src/DB/PermissionRepository.php
+++ src/DB/PermissionRepository.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace Admin\DB;
 
+use Nette\Utils\Arrays;
+
 /**
  * Class RoleRepository
  * @extends \StORM\Repository<\Admin\DB\Permission>
  */
 class PermissionRepository extends \StORM\Repository
 {
+	private const FREE_RESOURCES = [':Admin:Error4xx:default'];
+	
 	public function isAllowed(string $role, string $resource, ?int $privilege = null): bool
 	{
+		if (Arrays::contains(self::FREE_RESOURCES, $resource)) {
+			return true;
+		}
+		
 		return !$this->many()
 			->where("'$resource' LIKE CONCAT(REPLACE(this.resource, ':*', ''),'%')")
 			->where('this.privilege IS NULL OR :privilege IS NULL OR this.privilege & :privilege', ['privilege' => $privilege])
