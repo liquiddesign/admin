4d7e98bb5a6fc92727b60dbcbe170f39d9a139cf
petrk <petr@lqd.cz>
2025-05-20 11:26:09 +0200

    feat: add addContainer method to manage container instances

--- src/Controls/AdminContainer.php
+++ src/Controls/AdminContainer.php
@@ -8,6 +8,15 @@ class AdminContainer extends Container
 {
 	use AjaxComponentsTrait;
 
+	public function addContainer(int|string $name): Container
+	{
+		$control = new self();
+		$control->currentGroup = $this->currentGroup;
+		$this->currentGroup?->add($control);
+
+		return $this[$name] = $control;
+	}
+
 	/**
 	 * @return array<mixed>
 	 */
